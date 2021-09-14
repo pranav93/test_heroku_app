c5277f738db383238d0f073daef1e95d75ef9b9a
pranav93 <ambhore.pranav@gmail.com>
2021-09-14 15:04:09 +0530

    Added app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 {
-  "name":"Sample App",
-  "description": "Heroku test sample app"
+  "name": "Sample App",
+  "description": "Heroku test sample app",
+  "environments": {
+    "review": {
+      "addons": ["ormongo:2-wt"]
+    }
+  }
 }
